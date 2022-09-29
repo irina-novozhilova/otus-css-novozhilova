c163372318783949f682e5d66e2d3650eee69dcb
Irina Novozhilova <irina@nsvision.com>
2022-09-29 19:55:53 +0400

    Внесла правки в webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
     entry: "./src/index.js",
     mode: "development",
@@ -5,8 +6,31 @@ module.exports = {
         filename: "main.js",
     },
     module: {
-        rules: [{ test: /\.(js)$/, use: "babel-loader" }],
+        rules: [
+            {
+                test: /\.(js)$/,
+                use: "babel-loader"
+            },
+            {
+                test: /\.(css)$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(jpg|jpeg|png)$/i,
+                use: "file-loader",
+            },
+            {
+                test: /\.(svg)$/i,
+                use: "svg-inline-loader",
+            }
+        ],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "./src/index.html"
+        })
+    ],
     devServer: {
         compress: false,
         open: "/",
